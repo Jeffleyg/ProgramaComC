da13743cd948cf9994d5096d0c24ece431ab8fbf
Jeffleyg <jefleygarcon007@gmail.com>
2023-12-12 01:06:25 -0300

    Commit c

---
+++ matriz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int soma(int a, int b){
+    
+}
+
+int main(){
+
+    int A, B, Soma, res;
+
+    printf("Digite um valor: ");
+    scanf("%d", &A);
+
+    printf("Digite um valor: ");
+    scanf("%d", &B);
+
+    printf("\n escolha a operação: \n1 - Adição\n2- Substração");
+    scanf("%d", &res);
+
+    Soma = A + B;
+
+    printf("%d\n", Soma);
+
+   
+
+    return 0;
+}
---
+++ vetor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int A, B;
+
+    printf("Digite um valor: ");
+    scanf("%d", &A);
+
+    printf("Digite um valor: ");
+    scanf("%d", &B);
+
+    return 0;
+}
